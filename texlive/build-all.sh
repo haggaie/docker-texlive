58b110dfa28911fd5a1c98ab0c61833bd93ae953
Haggai Eran <haggai.eran@gmail.com>
2025-11-03 14:41:59 +0200

    build: allow build one-at-a-time and remove multi-arch from local builds

--- texlive/build-all.sh
+++ texlive/build-all.sh
@@ -2,20 +2,38 @@
 
 dir=$(dirname $0)
 
+PARALLEL=0
+
+# Parse command line arguments
+while getopts "p" opt; do
+    case $opt in
+        p) PARALLEL=1 ;;
+        *) echo "Usage: $0 [-p]" >&2
+           exit 1 ;;
+    esac
+done
+
 # Ensure buildx builder exists and is active
 docker buildx create --name multiarch --driver docker-container --use 2>/dev/null || docker buildx use multiarch
 
 for release in bookworm trixie ; do
-    docker buildx build \
-        --platform linux/amd64,linux/arm64 \
+    # --platform linux/amd64,linux/arm64 to create a multi-arch image
+    cmd=(docker buildx build \
         -t haggaie/texlive:$release \
         --build-arg RELEASE=$release \
-        -f $dir/Dockerfile \
+        -f "$dir/Dockerfile" \
         --load \
-        $dir &
+        "$dir")
+    if [ $PARALLEL -eq 1 ]; then
+        "${cmd[@]}" &
+    else
+        "${cmd[@]}"
+    fi
 done
 
-wait
+if [ $PARALLEL -eq 1 ]; then
+    wait
+fi
 
 # Tag bookworm as latest
 docker tag haggaie/texlive:trixie haggaie/texlive:latest
